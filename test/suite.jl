e78aa1a6b935a0ddeba814a347aa4086d6e4f8e2
Wei Tang <hi@that.world>
2016-07-22 14:45:11 +0200

    rearrange tests

--- test/suite.jl
+++ test/suite.jl
@@ -1,8 +1,12 @@
 import HTTP2
 
-# Server Testing
-@async HTTP2.serve(8080, b"<h1>Hello, world!</h1>")
-run("cd test/http2-test && npm install && grunt mochaTest:server")
+cd("test/http2-test")
+run(`npm install`)
 
 @async (headers, body) = HTTP2.request(ip"127.0.0.1", 8000, b"/")
-run("cd test/http2-test && npm install && grunt mochaTest:client")
+sleep(3)
+run(`grunt mochaTest:server`)
+
+@async HTTP2.serve(8080, b"<h1>Hello, world!</h1>")
+sleep(3)
+run(`grunt mochaTest:client`)
